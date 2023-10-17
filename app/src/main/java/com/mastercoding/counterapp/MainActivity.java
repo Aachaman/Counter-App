0f6402daef488d76f8a193883faff034126d5f62
Aachaman <aachamansinghchilwal@gmail.com>
2023-10-17 10:42:05 +0530

    commit

---
+++ app/src/main/java/com/mastercoding/counterapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mastercoding.counterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView welcome_text, counter_text;
+    Button btn;
+
+    int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        welcome_text = findViewById(R.id.welcome_text);
+        counter_text = findViewById(R.id.counter_text);
+
+        // Adding functionalities
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter_text.setText(""+increaseCounter());
+            }
+        });
+
+    }
+
+    public int increaseCounter(){
+        return ++counter;
+    }
+
+}
